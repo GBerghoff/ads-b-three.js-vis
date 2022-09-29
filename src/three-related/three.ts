7c12a81c5318cb88401cb2bec36531d87052bd0e
gberghoff <gageberghoff@gmail.com>
2022-09-28 22:01:58 -0400

    Temporary fix for resizing of the canvas

--- src/three-related/three.ts
+++ src/three-related/three.ts
@@ -82,3 +82,11 @@ export function addMultipleMeshToScene(meshs: any[]) {
     });
   }
 }
+
+
+window.addEventListener( 'resize', onWindowResize, false );
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
