804e456e42ef835b880b5b5b01837ba3a67d9955
gberghoff <gageberghoff@gmail.com>
2022-10-30 15:33:09 -0400

    initial lat lon positioning testing

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
-import { initialize, addMeshToScene, animate, animateMesh, createBox, createWorld, addMultipleMeshToScene } from "../three-related/three";
+import * as THREE from "three";
+import { initialize, animate, createWorld, addMultipleMeshToScene, createCustomBox, calcPosFromLatLonRad } from "../three-related/three";
 
 export default {
   mounted() {
     initialize("container");
     const earth = createWorld(5, 50, 50, './assets/8k_earth_nightmap.jpg');
+    const testBox = createCustomBox(.05, .05, .05, {color: new THREE.Color( 'skyblue' )}, calcPosFromLatLonRad(5, 90, 135))
     addMultipleMeshToScene([earth]);
+    addMultipleMeshToScene([testBox]);
     animate();
   },
 };
